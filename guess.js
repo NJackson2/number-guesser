4d65af703defa4464edd0c99c8c9bec51d702a1c
Nick Jackson <nickdjackson@msn.com>
2022-10-13 10:38:27 -0400

    third

--- guess.js
+++ guess.js
@@ -5,4 +5,32 @@
 // if it lower, say "too low" and ask again
 // if it's the right number, say congratulations and exit
 
-import readline from "realine-sync"
+import readline from "readline-sync"
+
+let userGuessedRight = false
+
+// while(true) {
+    let NumberChosen = Math.ceil(Math.random() * 10) //guess a number from 1-10
+    // console.log(NumberChosen)
+
+while(!userGuessedRight) {
+    let guess = readline.question("What is your guess?")
+    if (guess == NumberChosen) {
+        console.log("Congratulations")
+        userGuessedRight = true
+    } else if (guess > NumberChosen) {
+        console.log("Your guess is too high") 
+    } else if (guess < NumberChosen) {
+        console.log("Your guess is too low")
+    }
+    //console.log(guess)
+}
+
+// }
+
+// let NumberChosen = Math.ceil(Math.random() * 10)
+// console.log(NumberChosen)
+
+// while(!userGuessedRight) {
+//     console.log("What is your guess?")
+// }
