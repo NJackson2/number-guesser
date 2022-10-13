ca217e946e24e2fec55026a114eef746345095c3
Nick Jackson <nickdjackson@msn.com>
2022-10-13 09:35:37 -0400

    second

---
+++ guess.js
@@ -0,0 +1,8 @@
+
+// create a program that guesses a number from 1 to 10
+// then asks us to guess that number.
+// if it is higher say "too high" and ask again
+// if it lower, say "too low" and ask again
+// if it's the right number, say congratulations and exit
+
+import readline from "realine-sync"
